aa1592aec492d8d10d82e70165767f01f78f79d3
ctews <christoffer@jimdo.com>
2015-02-28 11:24:18 +0100

    Small test cleanup

--- SwiftMemCacheTests/SwiftMemCacheTests.swift
+++ SwiftMemCacheTests/SwiftMemCacheTests.swift
@@ -12,11 +12,6 @@ import XCTest
 
 class SwiftMemCacheTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
@@ -106,12 +101,4 @@ class SwiftMemCacheTests: XCTestCase {
         memCache.reset()
         XCTAssertEqual(memCache.size(), 0, "reset() test passed")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
